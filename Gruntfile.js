12f6ceae792169651b490908057a37414563cf4e
colinmiller <colinchmiller@gmail.com>
2016-06-09 21:48:29 -0500

    grunt and client side html and css functional

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      app: {
+        src: 'client/scripts/app.js',
+        dest: 'server/public/assets/scripts/app.min.js'
+      }
+    },
+    copy: {
+      html: {
+        expand: true,
+        cwd: 'client/views',
+        src: '*.html',
+        dest: 'server/public/assets/views'
+      },
+      styles: {
+        expand: true,
+        cwd: 'client/styles',
+        src: 'stylesheet.css',
+        dest: 'server/public/assets/styles'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['copy', 'uglify']);
+};
