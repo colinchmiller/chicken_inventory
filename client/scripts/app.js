28f8b11f700ad6c00fe1328c4e596bc81026ecb9
colinmiller <colinchmiller@gmail.com>
2016-06-14 09:30:26 -0500

    chicken form controller and binding of form elements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,6 @@
 var mainApp = angular.module('mainApp', ['ngRoute']);
+
+mainApp.controller('ChickenFormController', ['$scope', function($scope){
+  console.log("ChickenFormController is running");
+  $scope.firstname = 'First Name';
+}]);
