a07f4ea171db6bd338ae1d79d584edd00752a1ce
colinmiller <colinchmiller@gmail.com>
2016-06-30 22:07:04 -0500

    refining form logic

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,4 +3,5 @@ var mainApp = angular.module('mainApp', ['ngRoute']);
 mainApp.controller('ChickenFormController', ['$scope', function($scope){
   console.log("ChickenFormController is running");
   $scope.firstname = 'First Name';
+  $scope.chickcount = $scope.americauna;
 }]);
