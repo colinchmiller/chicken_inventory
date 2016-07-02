68b2b9e07e7b44acc3f0d64685cba52fed7784d4
colinmiller <colinchmiller@gmail.com>
2016-07-01 20:43:52 -0500

    add chick count

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,5 @@ var mainApp = angular.module('mainApp', ['ngRoute']);
 
 mainApp.controller('ChickenFormController', ['$scope', function($scope){
   console.log("ChickenFormController is running");
-  $scope.firstname = 'First Name';
-  $scope.chickcount = $scope.americauna;
+
 }]);
