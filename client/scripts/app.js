c211212b29d3e90404ad611871e1dd7cc01bc33b
colinmiller <colinchmiller@gmail.com>
2016-07-06 16:15:41 -0500

    add logic for chick total

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,21 @@
 var mainApp = angular.module('mainApp', ['ngRoute']);
 
 mainApp.controller('ChickenFormController', ['$scope', function($scope){
-  $scope.chickCount = $scope.americauna; 
+
+  $scope.americauna = 0;
+  $scope.bufforpington = 0;
+  $scope.barredrock = 0;
+  $scope.australorp = 0;
+  $scope.silverwyandotte = 0;
+  $scope.californiawhite = 0;
+  $scope.rhodeislandred = 0;
+  $scope.goldstar = 0;
+
+  $scope.chickensum = function() {
+    return $scope.americauna + $scope.bufforpington + $scope.barredrock +
+    $scope.australorp + $scope.silverwyandotte + $scope.californiawhite +
+    $scope.rhodeislandred + $scope.goldstar;
+
+  }
 
 }]);
